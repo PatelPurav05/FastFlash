6cd883465a95d59ef4be7c4caba98c885040585a
janani <jananip@uci.edu>
2024-08-18 06:04:58 -0700

    something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Water
+          Water haha
         </p>
         <a
           className="App-link"
