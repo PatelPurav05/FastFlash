d64e985e68536f21b713065c78c4a46a705a4708
Purav Patel <purav001@gmail.com>
2024-08-18 18:22:17 -0700

    Added endpoint to generate keywords and definitions from static pdf file

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,20 @@
 import time
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import google.generativeai as genai
+import os
+import typing_extensions as typing
 
+class Definitions(typing.TypedDict):
+  word: str
+  definition: str
+
+genai.configure(api_key=os.environ["API_KEY"])
+myfile = genai.upload_file("D:\Documents\Projects\FastFlash\\fast-flash\src\\backend\\testfile.pdf")
+model = genai.GenerativeModel('gemini-1.5-flash',
+                              # Set the `response_mime_type` to output JSON
+                              # Pass the schema object to the `response_schema` field
+                              generation_config={"response_mime_type": "application/json",
+                                                 "response_schema": list[Definitions]})
 app = Flask(__name__)
 
 @app.route('/get_definitions')
@@ -10,3 +24,9 @@ def get_definitions():
         'movies': 'includes storyline and characters',
     }
     return jsonify(definitions)
+
+@app.route('/get_gemini_response')
+def get_gemini_response():
+    response = model.generate_content(
+    [myfile, "\n\n", "Can you generate 10 keywords and definitions from this file?"])
+    return response.text
