fd31edab2e9c85bdbc024ce0cbb437565c94d233
Purav Patel <purav001@gmail.com>
2024-08-19 16:34:22 -0700

    File Upload only visible when user is authenticated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import FileUploadComponent from './components/FileUpload/FileUploadComponent';
-import Navbar, { StickyNavbar } from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import { useState, useEffect } from 'react';
-import { Auth } from './components/auth';
+import { auth } from './service/firebase';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(setUser);
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="App bg-gradient-to-r from-emerald-200 to-emerald-400 h-full">
       <Navbar />
+       
       <header className="App-header">
-        <FileUploadComponent />
+        {user &&
+          <FileUploadComponent />
+        }
       </header>
+      
     </div>
   );
 }
