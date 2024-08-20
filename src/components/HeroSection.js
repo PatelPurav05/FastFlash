8e73a16367c6235eb4f597f0938c00b784726a58
Purav Patel <purav001@gmail.com>
2024-08-19 17:56:03 -0700

    Positioned video above app background

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import '../App.css'
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
+    <div className='hero-container z-40'>
       <video src='/video/green-bg.mp4' autoPlay loop muted />
       <h1 class="text-lg font-bold transition-transform duration-300 hover:scale-110 hover:text-teal-400">
         FASTFLASH
